e9a112932844f58e2344f7b8b4d8c2870f34b2da
Synthia Kageni <synthiakageni@gmail.com>
2021-06-21 13:17:10 +0300

    add to scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,22 +1,51 @@
-$(".clickable1").click(function(){
-    $(".show1").slideToggle();
-    $(".hide1").Toggle();
-    $(".show1").slideToggle();
-    $(".hide1").fadeOut(1500);
+$(".clickable1").ready(function(){
+    $(".hide1").fadeOut();
+    $(".show1").on("click",function(){
+      $(".hide1").fadeIn();
+      $(".show1").slideToggle();
+    });
+    $(".hide1").on("click",function(){
+      $(".show1").show().fadeIn();
+      $(".hide1").fadeOut();
+    });
+    
   });
+
+  $(".clickable2").ready(function(){
+    $(".hide2").fadeOut();
+    $(".show2").on("click",function(){
+      $(".hide2").fadeIn();
+      $(".show2").slideToggle();
+    });
+    $(".hide2").on("click",function(){
+      $(".show2").show().fadeIn();
+      $(".hide2").fadeOut();
+    });
+    
+  });
+
+  $(".clickable3").ready(function(){
+    $(".hide3").fadeOut();
+    $(".show3").on("click",function(){
+      $(".hide3").fadeIn();
+      $(".show3").slideToggle();
+    });
+    $(".hide3").on("click",function(){
+      $(".show3").show().fadeIn();
+      $(".hide3").fadeOut();
+    });
+    
+  });
+
   
-  $(".clickable2").click(function() {
-    $(".hide2").slideToggle(1200);
-    $(".show2").slideToggle(1500);
-  });
-  $(".clickable3").click(function() {
-    $(".hide3").slideToggle(1200);
-    $(".show3").slideToggle(1500);
-  });
+  
+  
   $("#sub").click(function(event) {
     var name = $("#name").val();
     alert(name + " Thank you for replying");
     event.preventDefault();
   });
+
+
 
   
